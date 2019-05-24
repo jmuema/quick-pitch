0d4550e4459e01a885acd1f9d2a7dd7142a114a8
josephmuema <jmuema95@gmail.com>
2019-05-24 12:49:53 +0300

    Added code that imported flask script ad defined app

---
+++ manage.py
@@ -0,0 +1,27 @@
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager, Server
+from app import create_app, db
+from app.models import User, Pitch, Comment
+
+#
+app = create_app('production')
+
+manager = Manager(app)
+manager.add_command('server', Server)
+#
+
+#
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+
+@manager.shell 
+def make_shell_context():
+    """
+    high level support for doing this and that.
+    """
+    return dict(app=app, db=db, User=User, Pitch=Pitch, Comment=Comment)
+
+
+if __name__ == '__main__':
+    manager.run()
