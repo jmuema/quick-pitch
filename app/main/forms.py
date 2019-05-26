c8cdcf4bb9d9a83bb0dfe43baf0fcb8eb90509d3
josephmuema <jmuema95@gmail.com>
2019-05-26 15:14:34 +0300

    imported the flask forms

---
+++ app/main/forms.py
@@ -0,0 +1,3 @@
+from flask_wtf import FlaskForm  # Class to help us create a form class
+from wtforms import StringField, TextAreaField, SubmitField, SelectField
+from wtforms.validators import Required
