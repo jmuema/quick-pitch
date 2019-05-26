3d66a5ab6eeec2483ae76fde0b21305b8aae1499
josephmuema <jmuema95@gmail.com>
2019-05-26 15:08:08 +0300

    Created a class for registration andlogin form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,3 +2,27 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, ValidationError, BooleanField
 from wtforms.validators import Required, Email, EqualTo
 from ..models import User
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Your Email Address', validators=[Required(), Email()])
+    username = StringField('Enter your username', validators=[Required()])
+    password = PasswordField('Password', validators=[Required(), EqualTo(
+        'password_confirm', message='Your passwords must be the same')])
+    password_confirm = PasswordField(
+        'Confirm Passwords', validators=[Required()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self, data_field):
+        if User.query.filter_by(email=data_field.data).first():
+            raise ValidationError("Your Email has been used to open another account!")
+
+    def validate_username(self, data_field):
+        if User.query.filter_by(username=data_field.data).first():
+            raise ValidationError('The username entered is already in use')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[Required(), Email()])
+    password = PasswordField('Password', validators=[Required()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
